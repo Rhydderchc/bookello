56596f89b1ac6ef9827048e7f059efda28ec3377
Rhyddy <rhydderchc@gmail.com>
2021-05-13 00:43:40 -0600

    styles

---
+++ src/container/HomeScreen/styles.tsx
@@ -0,0 +1,20 @@
+const styles = {
+	wrapper: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#ffffff'
+	},
+	flatlist: {
+		paddingTop: 15,
+		paddingBottom: 10
+	},
+	message: {
+		fontSize: 16,
+		fontFamily: 'CircularLight',
+		marginBottom: 5
+	},
+	searchIcon: { paddingRight: 20 },
+	helpIcon: { paddingRight: 25 },
+	headerIconsWrapper: { flexDirection: 'row' }
+};
