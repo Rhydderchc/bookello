0a24995a5f912d99566e87081665d811cda9ea47
Rhydderchc <rhydderchc@gmail.com>
2021-05-13 20:33:58 -0600

    styles migration

---
+++ src/components/BookSearch/styles.tsx
@@ -0,0 +1,23 @@
+const styles = {
+	scrollView: { flex: 1 },
+	scrollViewContent: {
+		alignItems: 'flex-start',
+		justifyContent: 'center',
+		paddingBottom: 50
+	},
+	input: {
+		fontSize: 15,
+		height: 36,
+		width: '95%',
+		color: contrastColor,
+		paddingRight: 15,
+		paddingLeft: 15,
+		paddingTop: 0,
+		paddingBottom: 0,
+		borderWidth: 1,
+		borderColor: contrastColor,
+		borderRadius: 20
+	}
+};
+
+export default styles
