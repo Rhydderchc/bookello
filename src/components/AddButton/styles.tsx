3d28c43045d07d9879567f8cb89962f67d37c6be
Rhydderchc <rhydderchc@gmail.com>
2021-05-13 20:32:51 -0600

    styles for the button

---
+++ src/components/AddButton/styles.tsx
@@ -0,0 +1,21 @@
+const styles = {
+	view: {
+		backgroundColor: primaryColor,
+		position: 'absolute',
+		bottom: 20,
+		right: 20,
+		height: 60,
+		width: 60,
+		borderRadius: 30,
+		justifyContent: 'center',
+		alignItems: 'center',
+		zIndex: 1
+	},
+	icon: {
+		name: 'plus',
+		type: 'feather',
+		color: 'white',
+		size: 28
+	}
+};
+export default styles
