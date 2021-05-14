d8de7a907a405a4036225a33523d8fbdca37c627
Rhydderchc <rhydderchc@gmail.com>
2021-05-13 20:33:28 -0600

    updating the folder structure

--- src/components/BookSearch/index.tsx
+++ src/components/BookSearch/index.tsx
@@ -33,24 +33,3 @@ function BookSearch(props) {
 
 export default BookSearch;
 
-const styles = {
-	scrollView: { flex: 1 },
-	scrollViewContent: {
-		alignItems: 'flex-start',
-		justifyContent: 'center',
-		paddingBottom: 50
-	},
-	input: {
-		fontSize: 15,
-		height: 36,
-		width: '95%',
-		color: contrastColor,
-		paddingRight: 15,
-		paddingLeft: 15,
-		paddingTop: 0,
-		paddingBottom: 0,
-		borderWidth: 1,
-		borderColor: contrastColor,
-		borderRadius: 20
-	}
-};
