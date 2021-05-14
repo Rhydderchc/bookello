d26bdd29025c6d73bffd4edfd3b13c25b53b6b1a
Rhydderchc <rhydderchc@gmail.com>
2021-05-13 20:31:11 -0600

    epub styles

---
+++ src/container/ReadingScreen/styles.tsx
@@ -0,0 +1,11 @@
+const styles = {
+	wholeScreen: { flex: 1 },
+	headerIcon: { padding: 5 },
+	iconWrapper: {
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+		alignItems: 'center',
+		width: 100
+	}
+};
+export styles
