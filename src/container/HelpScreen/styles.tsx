bb300101c1846167f54dc763bd47a4b2e9fb0e0f
Rhydderchc <rhydderchc@gmail.com>
2021-05-13 20:28:59 -0600

    styling the help page

---
+++ src/container/HelpScreen/styles.tsx
@@ -0,0 +1,50 @@
+const styles = {
+	wrapper: {
+		flex: 1,
+		backgroundColor: '#ffffff'
+	},
+	wrapperContent: {
+		padding: 20,
+		paddingTop: 5,
+		paddingBottom: 40
+	},
+	emphasize: {
+		fontFamily: 'Circular',
+		fontSize: 17
+	},
+	text: {
+		fontFamily: 'CircularLight',
+		fontSize: 16,
+		lineHeight: 25,
+		marginTop: 10,
+		marginBottom: 10
+	},
+	heading: {
+		fontFamily: 'CircularBold',
+		marginTop: 15,
+		fontSize: 18
+	},
+	bullets: {
+		fontFamily: 'CircularLight',
+		fontSize: 16,
+		lineHeight: 25,
+		marginTop: 3,
+		marginBottom: 3
+	},
+	screenshot1: {
+		height: (width - 40) / 1.316,
+		width: width - 40,
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: 'rgba(0, 0, 0, 0.2)',
+		marginTop: 15,
+		marginBottom: 15
+	},
+	screenshot2: {
+		height: (width - 40) / 1.073,
+		width: width - 40,
+		borderRadius: 10,
+		marginTop: 15
+	}
+};
+export default styles
